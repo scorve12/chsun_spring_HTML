bfcfa0a559e99baaed34bf3d88c844291830bfbf
501Legion <gunner07050@gmail.com>
2024-06-03 22:32:00 +0900

    폰트 변경

--- header.php
+++ header.php
@@ -9,17 +9,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Jua&display=swap" rel="stylesheet">
     <style>
         .top-right {
             position: absolute;
             top: 100px;
             right: 10px;
-            font-size: 15.5px;
+            font-size: 19.5px;
+        }
+
+        .jua-regular {
+        font-family: "Jua", sans-serif;
+        font-weight: 500;
+        font-style: normal;
         }
     </style>
 </head>
 <body>
-    <div class="top-right">
+    <div class="top-right jua-regular">
         <?php if (isset($_SESSION['userid'])): ?>
             <b><?php echo $_SESSION['userid']; ?> <?php echo $role; ?></b>님 반갑습니다.
         <?php endif; ?>
