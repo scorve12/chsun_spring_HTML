affa5d71149ec7bee0c0c66684a8c74247a8f7cf
501Legion <gunner07050@gmail.com>
2024-06-10 03:06:02 +0900

    마지막

--- main/default.php
+++ main/default.php
@@ -60,7 +60,7 @@
     </header>
     <!--코드 작성-->
     <!--코드 작성-->
-    <footer>
+    <!-- <footer>
         <div class="contact-info">
             <p>(61452)광주광역시 동구 필문대로 309(서석동, 조선대학교)</p>
             <p>TEL. 062-230-6046 FAX. 062-232-0504</p>
@@ -70,7 +70,7 @@
             <p>과제 : 자율설계학기제</p>
             <p>마감 : 6월 8일</p>
         </div>
-    </footer>
+    </footer> -->
 
     <?php if (isset($_SESSION['userid'])): ?>
         <button class="fixed-button">
